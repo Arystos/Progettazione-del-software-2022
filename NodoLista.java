acca8ace970e649a69693004aa72f84412a7b355
Arystos <39024529+Arystos@users.noreply.github.com>
2022-10-27 14:33:36 +0200

    File NodoLista di Esercitazione5

---
+++ NodoLista.java
@@ -0,0 +1,11 @@
+package org.uniroma1.it.collections;
+
+public class NodoLista<E> {
+    NodoLista<E> next;
+    E elemento; // Informazione contenuta all'interno del nodo lista
+
+    NodoLista(NodoLista<E> nest, E elemento) {
+        this.next = next;
+        this.elemento
+    }
+}
